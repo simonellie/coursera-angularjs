4b4ebd707b6c5dbc01770c3f047166a111a9e74f
Simonelli Emanuele <e.simonelli1@gmail.com>
2017-12-01 15:15:44 +0100

    example lecture41

---
+++ examples/first-angularjs-sample/Lecture41/src/menuheader/menuheader.component.js
@@ -0,0 +1,37 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingList')
+.component('menuappHeader', {
+  templateUrl: 'src/menuheader/templates/menuheader.template.html',
+  controller: MenuAppHeaderController
+});
+
+MenuAppHeaderController.$inject = ['$rootScope'];
+function MenuAppHeaderController ($rootScope) {
+  var $ctrl = this;
+  var cancellers = [];
+
+  $ctrl.$onInit = function () {
+    var cancel = $rootScope.$on('$stateChangeSuccess',
+    function (event, toState, toParams, fromState, fromParams) {
+      if (toState.name.length) {
+        $("[ui-sref="+toState.name+"]").addClass("active");
+      }
+      if (fromState.name.length) {
+        $("[ui-sref="+fromState.name+"]").removeClass("active");
+      }
+    });
+    cancellers.push(cancel);
+  };
+
+  $ctrl.$onDestroy = function () {
+    cancellers.forEach(function (item) {
+      item();
+    });
+};
+
+};
+
+
+})();
